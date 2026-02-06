da28a3e6e7423f251c631c20dbd0a472c30dcecf
Clark Tagalog <tagalog.clark123@gmail.com>
2026-02-06 19:19:26 +0800

    Aligned backend endpoint to /submit to match frontend

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from typing import List
 from fastapi import FastAPI
+from sqlmodel import Session, SQLModel, create_engine
 from fastapi.middleware.cors import CORSMiddleware
-from sqlmodel import SQLModel, Session, create_engine
 from models import Survey
+
 
 sqlite_url = "sqlite:///survey.db"
 engine = create_engine(sqlite_url)
 
 app = FastAPI()
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -20,10 +22,15 @@ app.add_middleware(
 def on_startup():
     SQLModel.metadata.create_all(engine)
 
-@app.post("/submit")
+@app.get("/")
+def root():
+    return {"status": "Backend is ready!"}
+
+
+@app.post("/submit", response_model=Survey)
 def submit_survey(survey: Survey):
     with Session(engine) as session:
         session.add(survey)
         session.commit()
         session.refresh(survey)
-        return {"message": "Success!", "id": survey.id}
+        return survey
